be8b9e582caeeb6dc081267b1e8391110100287c
Nicolas JM <42520564+njm181@users.noreply.github.com>
2020-12-16 14:53:22 -0300

    update project

---
+++ src/main/java/com/gmdevs/backendweb/BackendwebApplication.java
@@ -0,0 +1,13 @@
+package com.gmdevs.backendweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmdevs/backendweb/BackendwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdevs.backendweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
