f74e326004b5c106de3a6cda9d6fc6171c1cdca2
Nicolas JM <42520564+njm181@users.noreply.github.com>
2020-12-16 17:37:37 -0300

    update

---
+++ src/main/java/com/gmdevs/backendweb/controller/RegistroController.java
@@ -0,0 +1,18 @@
+package com.gmdevs.backendweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registro")
+public class RegistroController {
+
+    @RequestMapping("")
+    public String getRegistro(Model model){
+        //List<Evento> listaEventos = reunionService.getReuniones();
+        //model.addAttribute("eventos", listaEventos);
+        return "reservar-lugar";
+    }
+
+}
